dc04f41e6aaa8d82686f1c04a55560ac44380c2e
allenlin90 <ht016746@gmail.com>
2022-08-17 09:28:25 +0700

    chore: init project

--- next.config.js
+++ next.config.js
@@ -1,7 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+// const withPWA = require('next-pwa');
+// eslint-disable-next-line
+const { i18n } = require('./next-i18next.config');
+// eslint-disable-next-line
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
 
-module.exports = nextConfig
+const nextConfig = {
+  // reactStrictMode: true,
+  i18n,
+  images: {
+    domains: ['flagcdn.com', 'images.unsplash.com'],
+  },
+  publicRuntimeConfig: {
+    production: process.env.APP_ENV === 'production',
+    staging: process.env.APP_ENV === 'staging',
+    local: process.env.APP_ENV === 'local',
+    MAPBOX_GL_ACCESS_TOKEN: process.env.MAPBOX_GL_ACCESS_TOKEN,
+  },
+  // pwa: {
+  //   disable: process.env.NODE_ENV !== 'production',
+  // },
+};
+
+// module.exports = withBundleAnalyzer(withPWA(nextConfig));
+module.exports = withBundleAnalyzer(nextConfig);
