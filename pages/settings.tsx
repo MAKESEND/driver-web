2919fa3f8bb06bcfc86ab08c2c87fbb440a20c0f
allenlin90 <ht016746@gmail.com>
2022-09-11 07:31:06 +0700

    style: maxwidth for setting

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -5,6 +5,7 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import DrawerLayout from 'components/layouts/drawerLayout/DrawerLayout';
 import Seo from 'components/common/Seo';
 import Settings from 'components/settings/Settings';
+import { MobileContainer } from 'components/common';
 
 export const getStaticProps: GetStaticProps = async ({ locale }) => {
   return {
@@ -19,7 +20,9 @@ export const SettingsPage: NextPageWithLayout = () => {
   return (
     <>
       <Seo title="Settings" />
-      <Settings />
+      <MobileContainer>
+        <Settings />
+      </MobileContainer>
     </>
   );
 };
