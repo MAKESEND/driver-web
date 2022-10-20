6696a90a1ffca065eb511eb9a188efb258ae676b
allenlin90 <ht016746@gmail.com>
2022-10-20 09:19:40 +0700

    fix: fix router redirect on root path

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import type { NextPage, GetStaticProps } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
+import { useRecoilValue } from 'recoil';
+import { userDataState } from 'states';
 
 import dynamic from 'next/dynamic';
 const Seo = dynamic(() => import('components/common/Seo'));
@@ -17,9 +19,15 @@ export const getStaticProps: GetStaticProps = async ({ locale }) => {
 
 const Home: NextPage = () => {
   const router = useRouter();
+  const userData = useRecoilValue(userDataState);
+
   useEffect(() => {
-    router.push('/auth/login');
-  });
+    if (userData) {
+      router.push('/dashboard');
+    } else {
+      router.push('/auth/login');
+    }
+  }, [userData, router]);
 
   return (
     <>
