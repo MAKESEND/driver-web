04bddf556268112b30c28fe964f69e390f80932a
allenlin90 <ht016746@gmail.com>
2022-10-28 10:11:03 +0700

    perf: optimize /settings

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -4,14 +4,11 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 import dynamic from 'next/dynamic';
 const Seo = dynamic(() => import('components/common/Seo'));
+const Settings = dynamic(() => import('components/settings/Settings'));
 const DrawerLayout = dynamic(
   () => import('components/layouts/drawerLayout/DrawerLayout'),
   { ssr: false }
 );
-const MobileContainer = dynamic(
-  () => import('components/common/mobile/MobileContainer')
-);
-const Settings = dynamic(() => import('components/settings/Settings'));
 
 export const getStaticProps: GetStaticProps = async ({ locale }) => {
   return {
@@ -26,15 +23,13 @@ export const SettingsPage: NextPageWithLayout = () => {
   return (
     <>
       <Seo title="Settings" />
-      <MobileContainer>
-        <Settings />
-      </MobileContainer>
+      <Settings />
     </>
   );
 };
 
 SettingsPage.getLayout = (page: React.ReactNode) => {
-  return <DrawerLayout>{page}</DrawerLayout>;
+  return <DrawerLayout mobileContainer>{page}</DrawerLayout>;
 };
 
 export default SettingsPage;
